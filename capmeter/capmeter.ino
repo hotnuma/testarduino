a112f908f6ee6d3bbfaf2b183cb7d5bc01fe7fad
hotnuma <hotnuma@gmail.com>
2023-08-21 15:22:19 +0200

    k factor

--- capmeter/capmeter.ino
+++ capmeter/capmeter.ino
@@ -56,7 +56,9 @@ double capa_LC(double L, double freq)
 
 double capa_555(double R1, double R2, double freq)
 {
-    return (1.44 / ( (R1 + (2 * R2)) * freq )) * 1e12;
+    double C = 1.44 / ((R1 + (2 * R2)) * freq);
+    double k = 1000/984.785;
+    return C * 1e12 * k;
 }
 
 bool first = true;
