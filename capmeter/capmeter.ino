0ee116793c46d3e0d21c08815dcf455a19286f38
hotnuma <hotnuma@gmail.com>
2023-09-13 17:46:50 +0200

    capmeter: LC mode 2

--- capmeter/capmeter.ino
+++ capmeter/capmeter.ino
@@ -54,9 +54,27 @@ double capa_RC(double R, double freq, double k)
     return (1 / (k * R * freq)) * 1e12;
 }
 
-double capa_LC(double part, double freq)
+//double capa_LC(double part, double freq)
+//{
+//    return (1 / (pow((2 * M_PI * freq), 2) * part));
+//}
+
+double LC_cal(double C2, double F2, double F1)
 {
-    return (1 / (pow((2 * M_PI * freq), 2) * part));
+    double FF2 = pow(F2, 2);
+    
+    double result = C2 * (FF2 / (pow(F1, 2) - FF2));
+    
+    printf("C1 = %f\n", result * 1e12);
+    
+    return result;
+}
+
+double LC_capa(double F, double F1, double C1, double k)
+{
+    //return (1 / (pow(F1, 2) * part));
+    
+    return C1 * ((pow(F1, 2) / pow(F, 2)) - 1.0) * k;
 }
 
 bool first = true;
@@ -67,8 +85,10 @@ double rc711_cal = 1231.305701;
 double rc711_k = 1.412463;
 
 // Mode 2 : LM711 LC
-double lc711_C1 = 1020e-12;
-double lc711_L = 100e-6;
+double C2 = 1000e-12;
+double F1 = 523773;
+double C1 = 1020e-12;
+double k = 0.95;
 
 void setup()
 {
@@ -118,14 +138,14 @@ void loop()
             
             if (digitalRead(CalPin) == LOW)
             {
-                lc711_C1 = capa_LC(lc711_L, gpsFreq.freq);
+                C1 = LC_cal(C2, gpsFreq.freq, F1);
                 printval(linestr, "Cal...", 3);
                 lcd.setCursor(0, 1);
                 lcd.print(linestr);
             }
             else
             {
-                double result = capa_LC(lc711_L, gpsFreq.freq) - lc711_C1;
+                double result = LC_capa(gpsFreq.freq, F1, C1, k);
                 dtostrf(result * 1e12, 1, 2, tempstr);
                 printval(linestr, tempstr, 3);
                 strcpy(linestr + (LSIZE-3), " pF");
