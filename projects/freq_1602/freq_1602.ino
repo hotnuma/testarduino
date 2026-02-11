79d2f9740c8d3666e627403d69eca7877fa61fee
hotnuma <hotnuma@gmail.com>
2026-02-11 10:12:48 +0100

    use 1602 lcd

--- projects/freq_1602/freq_1602.ino
+++ projects/freq_1602/freq_1602.ino
@@ -1,46 +1,35 @@
-// GPS Frequency Counter Example Sketch.
-// Connect the 1 PPS signal to INT0 (D2 pin).
-// Connect the input signal to be measured to T1 (D5 pin).
-// The highest frequency the counter can measure is limited to about
-// system clock frequency divided by 2.5, so about 6MHz with a
-// 16MHz clock.
-
-#include <Adafruit_SSD1306.h>
+#include <LiquidCrystal_I2C.h>
 #include <gpsFreq.h>
 
-Adafruit_SSD1306 display(128, 64, &Wire, -1);
+LiquidCrystal_I2C lcd = LiquidCrystal_I2C(0x27, 16, 2);
 
-uint16_t nSample;
+uint16_t count;
 char strFreq[16];
 
 void setup()
 {
-    display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
-    display.display();
+    lcd.init();
+    lcd.backlight();
 }
 
 void loop()
 {
-    if (!gpsFreq.isBusy)
-    {
-        if (nSample > 0)
-        {
-            display.clearDisplay();
-            display.setTextColor(WHITE);
-            display.setTextSize(2);
-            
-            display.setCursor(0, 0);
+    if (gpsFreq.isBusy)
+        return;
 
-            //gpsFreq.formatFreq(strFreq);
-            display.println(gpsFreq.freq);
-            
-            display.display();
-        }
-        
-        ++nSample;
-        
-        gpsFreq.start(1);
+    if (count > 0)
+    {
+        lcd.clear();
+        lcd.setCursor(2, 0);
+        gpsFreq.formatFreq(strFreq);
+        //lcd.print(gpsFreq.freq);
+        //lcd.print(strFreq);
+        lcd.printstr(strFreq);
     }
+    
+    ++count;
+    
+    gpsFreq.start(1);
 }
 
 
