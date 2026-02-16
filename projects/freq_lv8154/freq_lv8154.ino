137869497ccec11ce6634464e966ed48f7ea3dd3
hotnuma <hotnuma@gmail.com>
2026-02-16 16:12:47 +0100

    test shift register

--- projects/freq_lv8154/freq_lv8154.ino
+++ projects/freq_lv8154/freq_lv8154.ino
@@ -1,10 +1,10 @@
 #include "Arduino.h"
-//#include "counter.h"
-//#include "shiftreg.h"
-//#include "math.h"
+#include "counter.h"
+#include "shiftreg.h"
+#include "math.h"
 
 //CounterIC counter;
-//ShiftRegIC shift;
+ShiftRegIC shift;
 
 void setup()
 {
@@ -13,15 +13,15 @@ void setup()
     //counter.init(&shift, 10, 11, 12, 13);
     
     //             clk shld qh
-    //shift.init(1000, 4, 3, 2);
+    shift.init(1000, 4, 3, 2);
 }
 
 void loop()
 {
     Serial.println("test");
     
-    //~ uint32_t result = shift.readByte();
-    //~ Serial.println(result);
+    uint32_t result = shift.readByte();
+    Serial.println(result);
     
     delay(1000);
 }
