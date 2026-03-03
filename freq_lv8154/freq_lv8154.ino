3018c95b40cad5f54b9e4eac7135126514fe3e02
hotnuma <hotnuma@gmail.com>
2026-03-03 04:09:53 +0100

    lv8154: string formatting

--- freq_lv8154/freq_lv8154.ino
+++ freq_lv8154/freq_lv8154.ino
@@ -9,18 +9,24 @@
 LiquidCrystal_I2C lcd = LiquidCrystal_I2C(0x27, NCHARS, 2);
 ShiftRegIC sreg;
 char buffer[NCHARS + 1] = {0};
+char line[NCHARS + 1] = {0};
 
 void formatFreq(char *result, uint32_t freq)
 {
-    char temp[NCHARS + 1];
+    if (freq > 500000000)
+    {
+        *result = '\0';
+        return;
+    }
 
+    char temp[NCHARS + 1];
     ltoa(freq, temp, 10);
-    char *pf = temp;
+    char *p = temp;
     uint8_t len = strlen(temp);
     
     for (uint8_t i = 0; i < len; ++i)
     {
-        *result++ = *pf++;
+        *result++ = *p++;
         
         if ((len - i - 1) % 3 == 0 && i < len-1)
             *result++ = ' ';
@@ -33,11 +39,7 @@ void setup()
 {
     lcd.init();
     lcd.backlight();
-    
-    //        freq clk shld qh
     sreg.init(1000, 9, 8, 7);
-
-    //          shift gbu gbl gau gal
     counter.init(&sreg, 3, 4, 5, 6);
     counter.start();
 }
@@ -55,12 +57,13 @@ void loop()
         diff = now - last;
         last = now;
 
-        lcd.clear();
+        //lcd.clear();
         lcd.setCursor(0, 0);
-        //strcpy(buffer, "                ");
-        
+        strcpy(line, "                ");
         formatFreq(buffer, diff);
-        lcd.print(buffer);
+        uint8_t pos = 14 - strlen(buffer);
+        strcpy(line + pos, buffer);
+        lcd.print(line);
         
         counter.triggered = 0;
     }
