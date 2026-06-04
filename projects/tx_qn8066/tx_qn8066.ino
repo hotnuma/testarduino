f43a971f7bc1e925c261b90eb00777d4c0939417
hotnuma <hotnuma@gmail.com>
2026-06-04 20:07:10 +0200

    qn8066 : remove oled screen

--- projects/tx_qn8066/tx_qn8066.ino
+++ projects/tx_qn8066/tx_qn8066.ino
@@ -1,16 +1,11 @@
-#include <Adafruit_SSD1306.h>
 #include <QN8066.h>
-
 #define FREQUENCY 875
 
-Adafruit_SSD1306 display(128, 64, &Wire, -1);
 QN8066 tx;
 bool detected = false;
 
 void setup()
 {
-    display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
-    display.display();
     delay(500);
     
     detected = tx.detectDevice();
@@ -32,18 +27,6 @@ void setup()
 
 void loop()
 {
-    display.clearDisplay();
-    display.setTextColor(WHITE);
-    display.setCursor(0, 0);
-
-    display.setTextSize(2);
-    if (detected)
-        display.println("detected");
-    else
-        display.println("not detected");
-
-    display.display();
-
     delay(500);
 }
 
